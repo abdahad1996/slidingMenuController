d8884c8e3ec854665ceb1e206b087d7a32e5a7d4
Brian Voong <bhlvoong@gmail.com>
2018-09-26 14:23:12 -0700

    Ep 4 Add Child and Reveal/Hide Animations

---
+++ SlideOutMenuLBTA/Controllers/HomeController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  SlideOutMenuLBTA
+//
+//  Created by Brian Voong on 9/25/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .red
+        setupNavigationItems()
+    }
+    
+    let menuController = MenuController()
+    
+    fileprivate let menuWidth: CGFloat = 300
+    
+    @objc func handleOpen() {
+        // initial position
+        menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
+        let mainWindow = UIApplication.shared.keyWindow
+        mainWindow?.addSubview(menuController.view)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            // final position to animate our menuController object
+            self.menuController.view.transform = CGAffineTransform(translationX: self.menuWidth, y: 0)
+            
+        })
+        
+        // more links down in the desc below as to why you need to make addChild call when adding a second view controller inside of a root view controller
+        addChild(menuController)
+    }
+    
+    @objc func handleHide() {
+        print("Hiding menu...")
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            
+            // final position to animate our menuController object
+            self.menuController.view.transform = .identity
+        })
+        
+//        menuController.view.removeFromSuperview()
+//        menuController.removeFromParent()
+    }
+    
+    fileprivate func setupNavigationItems() {
+        navigationItem.title = "Home"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cellId")
+        
+        cell.textLabel?.text = "Row: \(indexPath.row)"
+        
+        return cell
+        
+    }
+
+}
+
