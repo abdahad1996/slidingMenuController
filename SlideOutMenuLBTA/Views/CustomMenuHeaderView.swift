fb64f74abcdd962df9f636bc4e314154c9f888d6
Brian Voong <bhlvoong@gmail.com>
2018-10-02 20:39:09 -0700

    Ep 12 Profile Image View Intrinsic Content Size

--- SlideOutMenuLBTA/Views/CustomMenuHeaderView.swift
+++ SlideOutMenuLBTA/Views/CustomMenuHeaderView.swift
@@ -9,25 +9,54 @@
 import UIKit
 
 class CustomMenuHeaderView: UIView {
-
+    
+    let nameLabel = UILabel()
+    let usernameLabel = UILabel()
+    let statsLabel = UILabel()
+    let profileImageView = ProfileImageView()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         backgroundColor = .white
-        
+        setupComponentProps()
+        setupStackView()
+    }
+    
+    fileprivate func setupComponentProps() {
         // custom components for our header
-        let nameLabel = UILabel()
         nameLabel.text = "Brian Voong"
         nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
-        let usernameLabel = UILabel()
         usernameLabel.text = "@buildthatapp"
-        
-        let statsLabel = UILabel()
         statsLabel.text = "42 Following 7091 Followers"
+        profileImageView.image = UIImage(named: "girl_profile")
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.clipsToBounds = true
+        profileImageView.backgroundColor = .red
         
+        setupStatsAttributedText()
+    }
+    
+    fileprivate func setupStatsAttributedText() {
+        statsLabel.font = UIFont.systemFont(ofSize: 14)
+        let attributedText = NSMutableAttributedString(string: "42 ", attributes: [.font: UIFont.systemFont(ofSize: 18, weight: .medium)])
+        attributedText.append(NSAttributedString(string: "Following  ", attributes: [.foregroundColor: UIColor.black]))
+        attributedText.append(NSAttributedString(string: "7091 ", attributes: [.font: UIFont.systemFont(ofSize: 17, weight: .medium)]))
+        attributedText.append(NSAttributedString(string: "Followers", attributes: [.foregroundColor: UIColor.black]))
+        
+        statsLabel.attributedText = attributedText
+    }
+    
+    fileprivate func setupStackView() {
         // this is a spacing hack with UIView
+        let rightSpacerView = UIView()
         let arrangedSubviews = [
-            UIView(), nameLabel, usernameLabel, SpacerView(space: 16), statsLabel
+            UIView(),
+            UIStackView(arrangedSubviews: [profileImageView, rightSpacerView]),
+            nameLabel,
+            usernameLabel,
+            SpacerView(space: 16),
+            statsLabel
         ]
         let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
         stackView.axis = .vertical
