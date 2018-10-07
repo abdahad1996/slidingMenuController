dd9677b3c067067386fcb178edb190daef19cca4
Brian Voong <bhlvoong@gmail.com>
2018-10-07 13:36:02 -0700

    Ep 19 Chatroom Menu Sections and Rows

--- SlideOutMenuLBTA/Controllers/Base/BaseSlidingController.swift
+++ SlideOutMenuLBTA/Controllers/Base/BaseSlidingController.swift
@@ -144,6 +144,7 @@ class BaseSlidingController: UIViewController {
     }
     
     var rightViewController: UIViewController = UINavigationController(rootViewController: HomeController())
+    let menuController = ChatroomsMenuController()
     
     fileprivate func performRightViewCleanUp() {
         rightViewController.view.removeFromSuperview()
@@ -189,7 +190,6 @@ class BaseSlidingController: UIViewController {
     }
     
     fileprivate func setupViewControllers() {
-        let menuController = MenuController()
         
         let homeView = rightViewController.view!
         let menuView = menuController.view!
---
+++ SlideOutMenuLBTA/Controllers/ChatroomsMenuController.swift
@@ -0,0 +1,51 @@
+//
+//  ChatroomsMenuController.swift
+//  SlideOutMenuLBTA
+//
+//  Created by Brian Voong on 10/7/18.
+//  Copyright © 2018 Brian Voong. All rights reserved.
+//
+
+import UIKit
+
+class ChatroomsMenuController: UITableViewController {
+    
+    let chatroomGroups = [
+        ["general", "introductions"],
+        ["jobs"],
+        ["Brian Voong", "Steve Jobs", "Tim Cook", "Barack Obama"]
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.separatorStyle = .none
+        
+        tableView.backgroundColor = #colorLiteral(red: 0.2980392157, green: 0.2078431373, blue: 0.2862745098, alpha: 1)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "UNREADS" : section == 1 ? "CHANNELS" : "DIRECT MESSAGES"
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return chatroomGroups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatroomGroups[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        // so what is the text to fill out?
+        let text = chatroomGroups[indexPath.section][indexPath.row]
+        cell.textLabel?.text = text
+        cell.backgroundColor = .clear
+        cell.textLabel?.textColor = .white
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        return cell
+    }
+
+}
