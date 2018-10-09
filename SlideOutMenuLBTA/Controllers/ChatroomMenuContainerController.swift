ff6bceccce9f80e0689c0dd56b99b88b5f04bccb
Brian Voong <bhlvoong@gmail.com>
2018-10-09 00:17:43 -0700

    SearchContainerView refactor

--- SlideOutMenuLBTA/Controllers/ChatroomMenuContainerController.swift
+++ SlideOutMenuLBTA/Controllers/ChatroomMenuContainerController.swift
@@ -12,8 +12,7 @@ class ChatroomMenuContainerController: UIViewController {
     
     let chatroomsMenuController = ChatroomsMenuController()
     
-    let searchContainer = UIView()
-    let rocketImageView = UIImageView(image: #imageLiteral(resourceName: "rocket"))
+    let searchContainer = SearchContainerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,31 +25,46 @@ class ChatroomMenuContainerController: UIViewController {
         view.addSubview(searchContainer)
         searchContainer.backgroundColor = #colorLiteral(red: 0.2549019608, green: 0.1843137255, blue: 0.2470588235, alpha: 1)
         
-        rocketImageView.contentMode = .scaleAspectFit
-        rocketImageView.layer.cornerRadius = 3
-        rocketImageView.clipsToBounds = true
-        view.addSubview(rocketImageView)
-        rocketImageView.anchor(top: nil, leading: view.leadingAnchor, bottom: searchContainer.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 8, bottom: 8, right: 0), size: .init(width: 44, height: 44))
-        
-        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).textColor = .white
-        
-        let searchBar = UISearchBar()
-        searchContainer.addSubview(searchBar)
-        searchBar.searchBarStyle = .minimal
-        searchBar.placeholder = "Enter some filer"
-        searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: searchContainer.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 4, right: 0))
-        
         searchContainer.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
         searchContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64).isActive = true
         
-        
         chatroomsView.anchor(top: searchContainer.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
-        
-//        chatroomsView.fillSuperview()
     }
 
 }
 
 class SearchContainerView: UIView {
+    
+    let searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.searchBarStyle = .minimal
+        sb.placeholder = "Enter some filer"
+        return sb
+    }()
+    
+    let rocketImageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "rocket"))
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 5
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).textColor = .white
+        
+        addSubview(searchBar)
+        addSubview(rocketImageView)
+        
+        rocketImageView.anchor(top: nil, leading: safeAreaLayoutGuide.leadingAnchor, bottom: bottomAnchor, trailing: nil, padding: .init(top: 0, left: 10, bottom: 10, right: 0), size: .init(width: 44, height: 44))
+        
+        searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 4, right: 0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
     
 }
