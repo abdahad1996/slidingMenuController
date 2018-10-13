f87eb592e42c792aaf862c609d6dc04d671987c0
Brian Voong <bhlvoong@gmail.com>
2018-10-13 13:02:57 -0700

    Ep 23 Circular Nav Bar Button

--- SlideOutMenuLBTA/Controllers/HomeController.swift
+++ SlideOutMenuLBTA/Controllers/HomeController.swift
@@ -29,8 +29,41 @@ class HomeController: UITableViewController, UIGestureRecognizerDelegate {
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Home"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        
+        setupCircularNavigationButton()
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupCircularNavigationButton() {
+        let image = #imageLiteral(resourceName: "girl_profile").withRenderingMode(.alwaysOriginal)
+        
+        let customView = UIButton(type: .system)
+//        customView.backgroundColor = .orange
+        customView.addTarget(self, action: #selector(handleOpen), for: .touchUpInside)
+//        customView.imageView?.image // this is not what you want
+        customView.setImage(image, for: .normal)
+        customView.imageView?.contentMode = .scaleAspectFit
+        
+        customView.layer.cornerRadius = 20
+        customView.clipsToBounds = true
+        
+        // this doesn't work
+        // custom view uses auto layout to put itself in the nav bar
+//        customView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        
+        customView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        customView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        let barButtonItem = UIBarButtonItem(customView: customView)
+        
+        navigationItem.leftBarButtonItem = barButtonItem
+        
+        // option #1 that doesn't work
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(handleOpen))
+        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
